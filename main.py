aaba752c7677b91b724eb7ff20f22877eee639a8
alvaro7 <alvaro20snbr@gmail.com>
2022-04-11 13:16:59 +0200

    prueba 1

---
+++ main.py
@@ -0,0 +1,127 @@
+import sys
+import pygame as pygame
+import serial
+import threading
+import utm
+
+# Huso para la conversión
+HusoHorario = 30
+
+# Variables globales
+gData = [0, 0]
+cerrado = 0
+
+
+# Para usar correctamente las variables gloabales, usamos estas dos funciones, como solo la funcion que lee escribe,
+# no hay region critica
+def setcerrar(dato):
+    global cerrado
+    cerrado = dato
+
+
+def getcerrar():
+    global cerrado
+    return cerrado
+
+
+def getdata():
+    global gData
+    return gData
+
+
+def setdata(data):
+    global gData
+    gData = data
+
+
+# Función que pasa grados y minutos a solo grados
+def fullgrade(grados, minutos):
+    minutos = minutos / 60.0
+    return grados + minutos
+
+
+def cambiarsigno(grados: int, letra: str):
+    if letra == 'W' or letra == 'S':
+        grados = -grados
+    return grados
+
+
+# Función que se va a ejecutar en otro thread
+# y que guardará los datos del serial en una variable global
+def GetData():
+    # puerto serial
+    with serial.Serial('/dev/ttyUSB0', 4800, timeout=1, parity=serial.PARITY_NONE, rtscts=1) as ser:
+        ser.flush()
+        while True:
+            if ser.inWaiting():
+                line = str(ser.readline())
+                # Si la línea tiene 'GPGGA' la parseamos y extraemos el valor
+                if line.split(',')[0] == "b'$GPGGA" and line.split(',')[6] != '0':
+                    splitline = line.split(',')[2:6]
+
+                    # Conversion de grados a utm
+                    aux = utm.from_latlon(
+                        cambiarsigno(fullgrade(float(splitline[0][0:2]), float(splitline[0][2::])), splitline[1]),
+                        cambiarsigno(fullgrade(float(splitline[2][0:3]), float(splitline[2][3::])), splitline[3]),
+                        HusoHorario, 'T')
+                    y = aux[0]
+                    x = aux[1]
+                    res = [x, y]
+                    setdata(res)
+                # hay que cerrar cada thread si queremos salir ademas de cerrar los puertos
+                if getcerrar() == 1:
+                    ser.close()
+                    sys.exit()
+
+
+# Función que actualizará los datos de la imágen en un hilo distinto
+
+def update_line():
+    color = (0, 255, 0)  # Color con el que dibujamos la linea
+    imagen = pygame.image.load("/home/alvaro/imagenGPS.jpg")  # Imagen de la pantalla,
+
+    # abrir display con el tamaño de la imagen, escalado
+    pantalla = pygame.display.set_mode((1024, 768))
+    # nombre de la pantalla
+    pygame.display.set_caption("GPS! ETSISI G4")
+    # Cargar imagen en el display
+    pygame.display.update()
+    pygame.time.Clock().tick(60)  # control del reloj de la imagen
+
+    while True:
+
+        for event in pygame.event.get():
+            # si cierran la pantalla salimos del programa o presionamos cualquier tecla tambien
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                setcerrar(1)
+                pygame.display.quit()
+                sys.exit()
+        datoantiguo = [0, 0]
+        data = getdata()
+        if data != datoantiguo:  # para no actualizar la pantalla de más, comprobamos si está en distinto sitio
+            pantalla.blit(imagen, (0, 0))
+            '''MIRAR ESTA PARTE'''
+            # dibujar punto en la imagen, dependiendo de la resolucion de la imagen, y la distancia de los extremos
+            # hay que restar o multiplicar por valores distintos
+            print("x" + str(1.13 * (-446782 + data[1])))
+            print("y" + str(1.8 * (-4470862 + data[0])))
+            pygame.draw.circle(imagen, color, ((-1.13 * (-446782 + data[1])), (1.8 * (-4470862 + data[0]))), 5, 0)
+            # actualizar display
+            pygame.display.update()
+        datoantiguo = data
+
+
+# Configuramos y lanzamos los hilo encargado de leer datos del serial y de actualizar pantalla
+
+dataCollector = threading.Thread(target=GetData, args=())
+displayupdater = threading.Thread(target=update_line, args=())
+dataCollector.start()
+displayupdater.start()
+
+dataCollector.join()
+displayupdater.join()
+
+# cerramos el programa completo en caso de que cerrar sea 1
+while True:
+    if getcerrar() == 1:
+        sys.exit()
